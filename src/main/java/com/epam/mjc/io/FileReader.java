b1b2c6b91c9cfeb13f4d83c2599924e1f7805bf2
Lapa <lapabig@gmail.com>
2023-05-30 22:13:20 +0600

    Updated

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,28 +5,17 @@ import java.io.*;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        BufferedReader br = null;
         String[] card = new String[4];
-        try {
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
             String line;
             int i = 0;
-            br = new BufferedReader(new java.io.FileReader(file));
             while ((line = br.readLine()) != null) {
                 String[] text = line.split(": ");
                 card[i] = text[1];
                 i++;
             }
-            br.close();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
         return new Profile(card[0], Integer.valueOf(card[1]), card[2], Long.valueOf(card[3]));
     }
